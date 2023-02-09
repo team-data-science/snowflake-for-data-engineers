5d503f057be9669b2b85c4d0276a9d756998d74d
Team Data Science Andreas Kretz <66935316+team-data-science@users.noreply.github.com>
2023-02-09 14:52:58 +0100

    Added direct select from S3
    Query data from S3 stage without importing it into a Snowflake table

--- worksheets/6_s3_integration.sql
+++ worksheets/6_s3_integration.sql
@@ -21,7 +21,7 @@
     ]
 }
 
--- AWS IAM role template:
+-- AWS IAM role trust relationship policy template:
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -40,7 +40,7 @@
   ]
 }
 
-
+-- create the storage integration for s3
 CREATE STORAGE INTEGRATION s3_storage_integration
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
@@ -51,13 +51,13 @@ CREATE STORAGE INTEGRATION s3_storage_integration
 -- to see the commando worked
 DESC INTEGRATION s3_storage_integration;
 
-
+-- create the s3 stage
 create or replace stage s3stage
   url = 's3://my-snowflake-store/staging/'
   file_format = ECOMMERCECSVFORMAT
   storage_integration = s3_storage_integration;
 
-
+-- create the new internal table
 create or replace TABLE TESTDB.ECOMMERCE.S3TABLE (
 	INVOICENO VARCHAR(38),
 	STOCKCODE VARCHAR(38),
@@ -69,11 +69,21 @@ create or replace TABLE TESTDB.ECOMMERCE.S3TABLE (
 	COUNTRY VARCHAR(20)
 );
 
+--list all files in the s3 stage
 list @s3stage;
 
+-- before you copy data into the table you can also read directly from the stage with the following select statement
+-- I didn't include this in the video, but it's super nice to have. This way you don't have to copy data into Snowflake first
+SELECT t.$1 as INVOICENO, t.$2 as STOCKCODE, t.$3 as DESCRIPTION
+from @S3STAGE (file_format => 'ECOMMERCECSVFORMAT') t;
+
+
+-- copy staged data into table
 copy into s3table
   from @s3stage;
 
+
+-- this is how you can remove a stage
 remove @s3stage;
 
 show tables;
